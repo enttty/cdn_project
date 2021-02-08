b5486b408988020740ebeef32cc9f9c85835c4d2
enttty <daniel@blackbox.localdomain>
2021-02-08 09:17:35 +0100

    sharing storage between pods seems to fail, try serving both UI on same pod

---
+++ docker_build/template/cdn-frontend/header.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>How to put PHP in HTML</title>
+</head>
+<body>
+<div>This is pure HTML message.</div>
+<div>Next, we’ll display today’s date and day using PHP!</div>
